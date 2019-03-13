c0f4e223e889f18f2167031c25e1794a8089fb7a
Egg4 <clement5444@yahoo.fr>
2019-03-13 22:03:36 +0100

    Change photo directory pattern

--- src/Resource/Photo/Controller.php
+++ src/Resource/Photo/Controller.php
@@ -12,7 +12,7 @@ class Controller extends \Egg\Controller\Generic
         if ($binary === false) {
             throw new InvalidContentException(sprintf('Url decode error'));
         }
-        $params['url'] = $this->createFile($binary);
+        $params['url'] = $this->createFile($binary, $params);
 
         return parent::create($params);
     }
@@ -22,7 +22,7 @@ class Controller extends \Egg\Controller\Generic
         $binary = $this->decodeUrl($params);
         if ($binary !== false) {
             $this->deleteFile($id);
-            $params['url'] = $this->createFile($binary);
+            $params['url'] = $this->createFile($binary, $params);
         }
 
         return parent::update($id, $params);
@@ -51,12 +51,13 @@ class Controller extends \Egg\Controller\Generic
         return $this->container['config']['photo']['dir'] . $url;
     }
 
-    protected function createFile($content)
+    protected function createFile($binary, array $params)
     {
         $authentication = $this->container['request']->getAttribute('authentication');
 
         $replacements = [
             '{entity_id}' => $authentication['entity_id'],
+            '{task_id}' => $params['task_id'],
             '{photo_hash}' => \Egg\Yolk\Rand::alphanum(32),
         ];
         $url = str_replace(
@@ -70,7 +71,7 @@ class Controller extends \Egg\Controller\Generic
         if (!file_exists($dirname)) {
             mkdir($dirname, 0777, true);
         }
-        file_put_contents($filename, $content);
+        file_put_contents($filename, $binary);
 
         return $url;
     }
@@ -80,5 +81,21 @@ class Controller extends \Egg\Controller\Generic
         $photo = $this->repository->selectOneById($id);
         $filename = $this->urlToFilename($photo->url);
         @unlink($filename);
+
+        // Remove 2 levels directories
+        $dirname = dirname($filename);
+        if (file_exists($dirname) AND $this->isDirEmpty($dirname)) {
+            rmdir($dirname);
+        }
+        $dirname = dirname($dirname);
+        if (file_exists($dirname) AND $this->isDirEmpty($dirname)) {
+            rmdir($dirname);
+        }
+    }
+
+    protected function isDirEmpty($dirname)
+    {
+        $iterator = new \FilesystemIterator($dirname);
+        return !$iterator->valid();
     }
 }
